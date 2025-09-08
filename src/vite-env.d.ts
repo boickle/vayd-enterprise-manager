b7238ff15ac1fcb4e1ba2c574fe2ca049cc83434
Brian Oickle <brianoickle@corp-1101.lan>
2025-09-08 09:55:19 -0400

    Add vite

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string
+    readonly VITE_MOCK_AUTH: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+  
